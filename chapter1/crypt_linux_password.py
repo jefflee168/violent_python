945b82d61ee7c96b5d917888c88c552c80b4fc7c
jefflee168 <86116193@qq.com>
2020-09-25 17:41:00 +0800

    add chapter2/portScan.py

--- chapter1/crypt_linux_password.py
+++ chapter1/crypt_linux_password.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python
+# coding: utf8
 
-import crypt
-
-def testPass(cryptPass):
-    salt = cryptPass[0:2]
-
-# 读取密码字典文件 dictionary.txt
-dicFile = open('dictionary.txt', 'r')
-
-for word in dicFile.readlines():
-    word = word.strip('\n')
-    cryptWord = crypt.crypt(word, salt)
-    if (cryptWord == cry)
